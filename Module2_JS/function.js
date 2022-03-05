2deb3989e22f88ad5ac730976222604ed62931d5
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-03-05 22:56:21 +0530

    IIFE

--- Module2_JS/function.js
+++ Module2_JS/function.js
@@ -7,3 +7,16 @@ let a = function addition(num1, num2) {
 };
 
 console.log(a(10, 60));
+
+// Immediately Invoked Function Expression -> IIFE
+// Immediately Invoked Means Calling it Right Away
+// It's Used Cuz if we know that we want to call it once then can call it at same place
+// Without Paramter
+
+(function () {
+  console.log("Hello World");
+})(/*functin calling right here inside this bracket*/);
+
+(function (num1, num2) {
+  console.log(num1 / num2);
+})(150, 15 /* Called Here Instead of Calling Somewhere else */);
