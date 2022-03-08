c5ab06ea27e7bd8b94f0e1849e31de5d1e1438df
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-03-08 21:53:20 +0530

    Object Start

---
+++ Module2_JS/object.js
@@ -0,0 +1,15 @@
+// An empty object
+let emptyobj = {};
+console.log(emptyobj);
+
+// Object -> (Object is Key:Value Pair) With Values
+// key is string by default in object
+let objec = {
+  Name: "Ujjwal Tiwari",
+  Age: 23,
+  Phone_No: 8894668683,
+  // If i don't Use Double Quotes in Key then it will still be treated as string
+  lastName: "Tiwari",
+};
+
+console.log(objec);
