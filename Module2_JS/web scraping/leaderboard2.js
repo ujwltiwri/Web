e095783774b6e32672dc867e588065c9231aed24
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-04-04 22:48:58 +0530

    data scraped for bastmans

--- Module2_JS/web scraping/leaderboard2.js
+++ Module2_JS/web scraping/leaderboard2.js
@@ -4,23 +4,21 @@ const { JSDOM } = jsdom;
 const link =
   "https://www.espncricinfo.com/series/ipl-2021-1249214/match-results";
 
+let leaderboard = [];
 request(link, cb);
 
-let leaderboard = []; //empty array of objects for storing player details
-// let nofplayers = 0; //to count no of players
-
-// let completelink[];
 function cb(error, response, html) {
   if (error) {
     console.log(error);
   } else {
     const dom = new JSDOM(html);
     const document = dom.window.document;
-    const allScoreCard = document.querySelectorAll('a[data-hover="Scorecard"]');
+    let scorecard = document.querySelectorAll('a[data-hover="Scorecard"]');
 
-    for (let i = 0; i < allScoreCard.length; i++) {
-      let allScoreCardlink = allScoreCard[i].href;
-      let completelink = "https://espncricinfo.com/" + allScoreCardlink;
+    for (let i = 0; i < scorecard.length; i++) {
+      let scorecardlink = scorecard[i].href;
+      let completelink = "https://www.espncricinfo.com/" + scorecardlink;
+      //getting details of batsman from everymatch using scorecard's link
       request(completelink, cb2);
     }
   }
@@ -32,26 +30,23 @@ function cb2(error, response, html) {
   } else {
     const dom = new JSDOM(html);
     const document = dom.window.document;
-    let batsmanRow = document.querySelectorAll(".table.batsman tbody tr");
-    for (let i = 0; i < batsmanRow.length; i++) {
-      let cells = batsmanRow[i].querySelectorAll("td");
+    let batsman = document.querySelectorAll(".table.batsman tbody tr");
+
+    for (let i = 0; i < batsman.length; i++) {
+      let cells = batsman[i].querySelectorAll("td");
       if (cells.length == 8) {
         let name = cells[0].textContent;
         let runs = cells[2].textContent;
         let balls = cells[3].textContent;
         let fours = cells[5].textContent;
         let sixes = cells[6].textContent;
+
         processPlayer(name, runs, balls, fours, sixes);
-        // nofplayers++;
       }
     }
+    console.log(leaderboard);
   }
 }
-
-processPlayer("MS Dhoni", 40, 24, 45, 3);
-processPlayer("MS Dhoni", 10, 34, 25, 30);
-processPlayer("MS Dhonis", 10, 34, 25, 30);
-console.log(leaderboard);
 
 function processPlayer(name, runs, balls, fours, sixes) {
   // because runs, balls, fours, sixes are string by default so if we add these then addition won't happen instead string will be concatenated
@@ -61,20 +56,7 @@ function processPlayer(name, runs, balls, fours, sixes) {
   fours = Number(fours);
   sixes = Number(sixes);
 
-  //if player's details exist for at least one time
-  for (let i = 0; i < leaderboard.length; i++) {
-    let playerObj = leaderboard[i]; //player object will store each player's details in key value form
-    if (playerObj.Name == name) {
-      playerObj.Runs += runs;
-      playerObj.Innings += 1;
-      playerObj.Balls += balls;
-      playerObj.Fours += fours;
-      playerObj.Sixes += sixes;
-      return;
-    }
-  }
-
-  //if player details doesn't exist in leaderboard array then save player details first
+  //if player details not found in leaderboard array
   let obj = {
     Name: name,
     Innings: 1,
@@ -84,5 +66,19 @@ function processPlayer(name, runs, balls, fours, sixes) {
     Sixes: sixes,
   };
 
-  leaderboard.push(obj); //so we pushed this object into the empty leaderboard array
+  for (let i = 0; i < leaderboard.length; i++) {
+    playerObj = leaderboard[i];
+
+    //checking if player details is found in leaderboard array on basis of their name
+    if (playerObj.Name == name) {
+      playerObj.Innings += 1;
+      playerObj.Runs += runs;
+      playerObj.Balls += balls;
+      playerObj.Fours += fours;
+      playerObj.Sixes += sixes;
+      return;
+    }
+  }
+
+  leaderboard.push(obj); // Player details pushed into array if player details is not found there
 }
